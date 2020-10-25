b13e6042d5770f41809311675cd8765c4642bf0c
Ashish Deora <ashish.deora@greatplacetowork.com>
2020-10-25 20:31:59 +0530

    Code changes for chapter 6 to 9

--- EmployeeManagementModels/Employee.cs
+++ EmployeeManagementModels/Employee.cs
@@ -10,7 +10,7 @@ namespace EmployeeManagementModels
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
-        public DateTime DateofBirth { get; set; }
+        public DateTime DateOfBirth { get; set; }
         public Gender Gender { get; set; }
         public Department Department { get; set; }
         public string PhotoPath { get; set; }
