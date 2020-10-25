067d5d039931235694c448528235c7146e2d298e
Ashish Deora <ashish.deora@greatplacetowork.com>
2020-10-25 22:05:46 +0530

    Migrations and Seed Data Inserted. Video 12

--- EmployeeManagement.Web/Pages/EmployeeListBase.cs
+++ EmployeeManagement.Web/Pages/EmployeeListBase.cs
@@ -26,7 +26,7 @@ namespace EmployeeManagement.Web.Pages
                 Email = "David@pragimtech.com",
                 DateOfBirth = new DateTime(1980, 10, 5),
                 Gender = Gender.Male,
-                Department = new Department { DepartmentId = 1, DepartmentName = "IT" },
+                DepartmentId = 1,
                 PhotoPath = "images/john.png"
             };
 
@@ -38,7 +38,7 @@ namespace EmployeeManagement.Web.Pages
                 Email = "Sam@pragimtech.com",
                 DateOfBirth = new DateTime(1981, 12, 22),
                 Gender = Gender.Male,
-                Department = new Department { DepartmentId = 2, DepartmentName = "HR" },
+                DepartmentId = 2,
                 PhotoPath = "images/sam.jpg"
             };
 
@@ -50,7 +50,7 @@ namespace EmployeeManagement.Web.Pages
                 Email = "mary@pragimtech.com",
                 DateOfBirth = new DateTime(1979, 11, 11),
                 Gender = Gender.Female,
-                Department = new Department { DepartmentId = 1, DepartmentName = "IT" },
+                DepartmentId = 3,
                 PhotoPath = "images/mary.png"
             };
 
@@ -62,7 +62,7 @@ namespace EmployeeManagement.Web.Pages
                 Email = "sara@pragimtech.com",
                 DateOfBirth = new DateTime(1982, 9, 23),
                 Gender = Gender.Female,
-                Department = new Department { DepartmentId = 3, DepartmentName = "Payroll" },
+                DepartmentId = 4,
                 PhotoPath = "images/sara.png"
             };
 
