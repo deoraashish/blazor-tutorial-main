c04f6940c850daf2bc688e692b48bbaed046b025
Ashish Deora <ashish.deora@greatplacetowork.com>
2020-10-25 17:48:42 +0530

    Add project files.

---
+++ EmployeeManagementModels/Department.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace EmployeeManagementModels
+{
+    public class Department
+    {
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+    }
+}
